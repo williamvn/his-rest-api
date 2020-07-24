d674cb3964d45a2e3b2c976d02d0c90ef4845e35
Willy <williamvn1995@gmail.com>
2020-07-24 10:11:46 +0200

    Mongoose Installed

--- src/users/controllers/patients.controller.ts
+++ src/users/controllers/patients.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Delete, Body, Param, Logger, HttpException, HttpStatus, Query } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param, Logger, HttpException, HttpStatus, Query, Res } from '@nestjs/common';
 import { Patient } from '../domain/patient.interface';
 import { PatientsService } from '../services/patients.service';
 import { PatientDTO } from '../DTO/patient.dto';
@@ -18,7 +18,7 @@ export class PatientController {
     findOne(@Param('id') id: number) {
         Logger.log("Get patient with id: " + id);
         var obj = this.patientService.getPatientById(id);
-        if(!obj){
+        if (!obj) {
             throw new HttpException("Item Not Found", HttpStatus.NOT_FOUND);
         }
         return obj;
@@ -43,10 +43,10 @@ export class PatientController {
     @Delete(':id')
     remove(@Param('id') id: number) {
         Logger.log("Delete Patient with Id: " + id);
-        if(!this.patientService.remove(id)){
+        if (!this.patientService.remove(id)) {
             throw new HttpException("item Not Found", HttpStatus.NOT_FOUND);
         }
-        else{
+        else {
             return {};
         }
     }
