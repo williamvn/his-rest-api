e4b31595fd3410f2878bf487c0318459458cdb41
Willy <williamvn1995@gmail.com>
2020-07-26 13:52:16 +0200

    swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const options = new DocumentBuilder()
+    .setTitle('HIS REST API')
+    .setDescription('HIS REST API Documentation')
+    .setVersion('1.0')
+    .addTag('his')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
