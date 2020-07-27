61158784241de20ae9f14091a09fbaf30412cea8
Willy <williamvn1995@gmail.com>
2020-07-27 14:37:08 +0200

    Password encrypted

--- src/app-users/controllers/app-users.controller.ts
+++ src/app-users/controllers/app-users.controller.ts
@@ -11,5 +11,4 @@ export class AppUsersController {
     registerUser(@Body() appUserDto: AppUserDTO):Promise<AppUser>{
         return this.appUserService.registerUser(appUserDto);
     }
-
 }
--- src/app-users/services/app-users.service.ts
+++ src/app-users/services/app-users.service.ts
@@ -3,13 +3,16 @@ import { AppUser } from '../domain/app-user.interface';
 import { AppUserDTO } from '../DTO/app-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import * as bcrypt from "bcrypt";
+
 
 @Injectable()
 export class AppUsersService {
 
     constructor(@InjectModel("AppUsers") private appUserModel: Model<AppUser> ){}
 
-    registerUser(appUser: AppUserDTO):Promise<AppUser>{
+    async registerUser(appUser: AppUserDTO):Promise<AppUser>{
+        appUser.password = await bcrypt.hash(appUser.password, 10);
         const newAppUser = new this.appUserModel(appUser);
         return newAppUser.save();
     }
